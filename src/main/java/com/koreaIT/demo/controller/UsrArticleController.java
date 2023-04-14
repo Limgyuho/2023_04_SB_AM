26c1bb8c074cbe4462c3b11ea46f0816e56f443e
Limgyuho <radum0719@hanmail.net>
2023-04-14 16:06:50 +0900

    ArticleService, ArticleRepository

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -1,85 +1,41 @@
-
 package com.koreaIT.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.koreaIT.demo.service.ArticleService;
 import com.koreaIT.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
 	
-	private int lastArticleId;
-	private List<Article> articles;
+	private ArticleService articleService;
 	
-	public UsrArticleController(){
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
-		
-		makeTestData();
+	@Autowired
+	public UsrArticleController(ArticleService articleService) {
+		this.articleService = articleService;
 	}
 	
-	// 서비스 메서드
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			
-			String title = "제목" + i;
-			String body = "내용" + i;
-			
-			writeArticle(title, body);
-		}
-	}
-	
-	private Article writeArticle(String title, String body) {
-		int id = this.lastArticleId + 1;
-		this.lastArticleId = id;
-		
-		Article article = new Article(id, title, body);
-		
-		articles.add(article);
-		
-		return article;
-	}
-	
-	private Article getArticleById(int id) {
-		for(Article article : articles) {
-			if(article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-	
-	private void deleteArticle(int id) {
-		
-		Article article = getArticleById(id);
-		
-		articles.remove(article);
-	}
-	
-
-	
-	private void modifyArticle(int id, String title, String body) {
-		Article article = getArticleById(id);
-		
-		//article 안에 있는 것들을 가져 와야 한다
-		article.setBody(body);
-		article.setTitle(title);
-		
-	}
-	
-	
-
 	// 액션 메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
+		return articleService.writeArticle(title, body);
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
 		
-		Article article = writeArticle(title, body);
+		Article article = articleService.getArticleById(id);
+		
+		if(article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
 		
 		return article;
 	}
@@ -87,58 +43,36 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
+		return articleService.getArticles();
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
 		
-		return this.articles;
+		Article article = articleService.getArticleById(id);
+		
+		if(article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		
+		articleService.modifyArticle(id, title, body);
+		
+		return id + "번 게시물을 수정했습니다";
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
 		
-		Article article = getArticleById(id);
+		Article article = articleService.getArticleById(id);
 		
 		if(article == null) {
 			return id + "번 게시물은 존재하지 않습니다.";
 		}
 		
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 		
 		return id + "번 게시물을 삭제했습니다";
 	}
-	
-	
-	@RequestMapping("/usr/article/doModify")
-	@ResponseBody
-	public String doModify(int id, String title, String body) {
-		
-		Article article = getArticleById(id);
-		 
-		if(article == null) {
-			
-			return id + "번 게시물은 존재하지 않습니다.";
-		}
-		
-		modifyArticle(id, body, title);
-		
-		return id + "번 글이 수정되었습니다";
-	}
-	
-	
-	@RequestMapping("/usr/article/showDetail")
-	@ResponseBody
-	public Object getArticle(int id, String title, String body) {
-		
-		
-		Article article = getArticleById(id);
-		 
-		if(article == null) {
-			
-			return id + "번 게시물은 존재하지 않습니다.";
-		}
-		
-		return article;
-		
-	}
-
-	
 }
