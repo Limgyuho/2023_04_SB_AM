8b9fc63f77e0de86cbf236ea3a5f78b04940897a
Limgyuho <radum0719@hanmail.net>
2023-04-18 14:36:18 +0900

    Util.empty()

--- src/main/java/com/koreaIT/demo/util/Util.java
+++ src/main/java/com/koreaIT/demo/util/Util.java
@@ -2,4 +2,21 @@ package com.koreaIT.demo.util;
 
 public class Util {
 
+	
+	public static boolean empty(Object obj) {
+		
+		if(obj == null) {
+			return true;
+		}
+		
+//		if(obj instanceof String == false) {
+//			
+//		}
+		
+		String str =(String) obj;
+		
+		
+		
+		return str.trim().length() == 0;
+	}
 }
